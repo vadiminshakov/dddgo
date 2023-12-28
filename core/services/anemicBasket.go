6c3dd33ce8a86376f44ab071d8dc63de09b193e3
Vadim Inshakov <vadiminshakov@gmail.com>
2023-12-29 01:45:25 +0500

    add items repo operations

--- core/services/anemicBasket.go
+++ core/services/anemicBasket.go
@@ -16,13 +16,21 @@ const (
 	maxBasketWeight = 10000 // 10 кг
 )
 
+type ItemsRepository interface {
+	// GetBasketItems возвращает товары в корзине
+	GetByBasketID(id int64) (*vos.BasketItem, error) // Save сохраняет корзину
+	// Save сохраняет позицию в корзине
+	Save(basket *vos.BasketItem) error
+}
+
 type anemicBasketService struct {
 	basketRepo BasketRepository
+	itemsRepo  ItemsRepository
 	producer   broker.Producer
 }
 
-func NewAnemicBasketService(basketRepo BasketRepository) *anemicBasketService {
-	return &anemicBasketService{basketRepo: basketRepo}
+func NewAnemicBasketService(basketRepo BasketRepository, itemsRepo ItemsRepository) *anemicBasketService {
+	return &anemicBasketService{basketRepo: basketRepo, itemsRepo: itemsRepo}
 }
 
 func (s *anemicBasketService) AddItem(basketID int64, item vos.BasketItem) error {
@@ -39,14 +47,26 @@ func (s *anemicBasketService) AddItem(basketID int64, item vos.BasketItem) error
 		return ErrFullBasket
 	}
 
+	var itemExists bool
 	for i, itemFromBasket := range basket.Items {
 		if itemFromBasket.GoodID == item.GoodID {
+			itemExists = true
 			basket.Items[i].Quantity += item.Quantity
 		}
 	}
-
-	basket.Items = append(basket.Items, vos.BasketItem{GoodID: item.GoodID, Quantity: item.Quantity})
 	basket.TotalWeight += item.Weight
+
+	if !itemExists {
+		basket.Items = append(basket.Items, &vos.BasketItem{GoodID: item.GoodID, Quantity: item.Quantity})
+	}
+
+	for _, itemForSave := range basket.Items {
+		if err := s.itemsRepo.Save(itemForSave); err != nil {
+			return err
+		}
+	}
+
+	// oops! we forgot to set basketID to item :(
 
 	if err := s.basketRepo.Save(basket); err != nil {
 		return err

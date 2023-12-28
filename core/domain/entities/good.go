3229faf72c9c921941bfb628ba0d74c0e5d74cfc
Vadim Inshakov <vadiminshakov@gmail.com>
2023-12-29 00:58:56 +0500

    add stub entity

---
+++ core/domain/entities/good.go
@@ -0,0 +1,19 @@
+package entities
+
+type Good struct {
+	ID    int64
+	Name  string
+	Price float64
+}
+
+func (g *Good) X() {
+	// TODO: implement
+}
+
+func (g *Good) Y() {
+	// TODO: implement
+}
+
+func (g *Good) Z() {
+	// TODO: implement
+}

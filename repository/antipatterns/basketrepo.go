e5cfcee9d5a75ed7d15f3bf133aee72a16e4014a
Vadim Inshakov <vadiminshakov@gmail.com>
2024-01-21 15:25:59 +0500

    add antipattern tx repo

---
+++ repository/antipatterns/basketrepo.go
@@ -0,0 +1,31 @@
+package antipatterns
+
+import (
+	"database/sql"
+	"examplegood/core/domain/aggregates"
+	"examplegood/repository"
+	"examplegood/repository/queries"
+)
+
+type TxBasket struct {
+	db *sql.DB
+	tx repository.Tx
+}
+
+func NewTxBasketRepo(db *sql.DB, tx repository.Tx) *TxBasket {
+	if tx == nil {
+		return &TxBasket{tx: db}
+	}
+	return &TxBasket{tx: tx}
+}
+
+func (r *TxBasket) GetByID(tx *sql.Tx, id int64) (*aggregates.Basket, error) {
+	tx.Exec(queries.BasketSave)
+	return &aggregates.Basket{}, nil
+}
+
+func (r *TxBasket) Save(tx *sql.Tx, basket *aggregates.Basket) error {
+	_, err := tx.Exec(queries.BasketSave, basket.ID, basket.TotalWeight)
+
+	return err
+}
